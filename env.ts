79c9aca4b9c2beddd4f5984f73e4c9cd67fecfe0
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2025-02-28 18:28:47 +0000

    add env setup

---
+++ env.ts
@@ -0,0 +1,43 @@
+/* eslint-disable node/no-process-env */
+/* eslint-disable ts/no-redeclare */
+
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z, ZodError } from "zod";
+
+const EnvSchema = z.object({
+  NODE_ENV: z
+    .union([
+      z.literal("localhost"),
+      z.literal("dev"),
+      z.literal("staging"),
+      z.literal("production"),
+    ])
+    .default("dev"),
+  HOST: z.string().default("127.0.0.1"),
+  PORT: z.string().default("3000"),
+});
+
+export type EnvSchema = z.infer<typeof EnvSchema>;
+
+expand(config());
+
+try {
+  EnvSchema.parse(process.env);
+}
+catch (error) {
+  if (error instanceof ZodError) {
+    let message = "Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += `${issue.path[0]}\n`;
+    });
+    const e = new Error(message);
+    e.stack = "";
+    throw e;
+  }
+  else {
+    console.error(error);
+  }
+}
+
+export default EnvSchema.parse(process.env);
