18fe973522afa8f784b23ab5519d9d1c19de6179
Juan Vasquez <37302562+simplyjuanc@users.noreply.github.com>
2025-03-06 15:23:38 +0000

    rename health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { createOpenAPIApp, registerApiRoutes } from "@/utils/app-utils";
 
 import collectionRouter from "./router/collection/collection.index";
-import healthRouter from "./router/index.routes";
+import healthRouter from "./router/health.routes";
 import { configureOpenAPI } from "./utils/open-api-utils";
 
 const app = createOpenAPIApp();
